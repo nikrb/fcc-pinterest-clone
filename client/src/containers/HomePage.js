367594fb59f5c040548f4e683589e4c34a3d3478
nikrb <nik_rb@yahoo.com>
2017-09-17 20:33:48 +0100

    add some images and wrap data grid

--- client/src/containers/HomePage.js
+++ client/src/containers/HomePage.js
@@ -7,12 +7,14 @@ export default class HomePage extends React.Component {
     const bu = "https://via.placeholder.com/";
     const data = [
       bu+"350x100", bu+"100x350", bu+"350x100", bu+"250x250", bu+"250x350",
-      bu+"250x150"
+      bu+"250x150", bu+"100x100", bu+"100x100", bu+"100x100", bu+"100x100"
     ];
     return (
       <div className="App">
         <h1>pcloneInterest</h1>
-        <PIGrid data={data} />
+        <div className="image_grid">
+          <PIGrid data={data} />
+        </div>
       </div>
     );
   };
