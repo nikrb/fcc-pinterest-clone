a92bf7cfd9d02303d53b357f990fda1d0bb625bb
nikrb <nik_rb@yahoo.com>
2017-09-17 20:39:56 +0100

    get a grid displayed

--- client/src/components/PIGrid.js
+++ client/src/components/PIGrid.js
@@ -3,13 +3,23 @@ import React from 'react';
 export default class PIGrid extends React.Component {
   render = () => {
     const {data} = this.props;
-    const pix = data.map( (p) => {
+    const heart = String.fromCharCode( 10084);
+    const heart_style = { color: "red"};
+    const card_style = { margin: "5%"};
+    const image_style = { width: "100%", borderRadius: "5px"};
+    const pix = data.map( (p,i) => {
       return (
-        <img className="item" src={p} alt="missing" />
+        <div className="item" key={i} >
+          <div style={card_style} >
+            <img style={image_style} src={p} alt="missing" />
+            <p>some text</p>
+            <button className="heart_button" type="button" ><span style={heart_style}>{heart}</span></button>
+          </div>
+        </div>
       );
     });
     return (
-      <div className="container">
+      <div className="masonry">
         {pix}
       </div>
     );
