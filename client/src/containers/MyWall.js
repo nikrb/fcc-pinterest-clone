189732b1e5f2dfa3d9bf40584248514113af61d0
nikrb <nik_rb@yahoo.com>
2017-09-18 15:04:43 +0100

    use new image dialogue in my wall

--- client/src/containers/MyWall.js
+++ client/src/containers/MyWall.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import Auth from '../modules/Auth';
 import ImageWall from '../components/ImageWall';
+import NewImageDialogue from '../components/NewImageDialogue';
 
 export default class MyWall extends React.Component {
+  state = {
+    show_new_image_dialogue: false
+  };
+  onAddImageClick = (e) => {
+    this.setState( {show_new_image_dialogue: true});
+  };
+  onNewImageCancel = () => {
+    this.setState( {show_new_image_dialogue: false});
+  };
+  onNewImageOK = (image_url) => {
+    // create new image for user
+    console.log( "add new image:", image_url);
+    this.setState( {show_new_image_dialogue: false});
+  };
   render = () => {
     const user_id = Auth.get_id();
     // generate a list of pics
@@ -14,7 +29,13 @@ export default class MyWall extends React.Component {
     return (
       <div className="App">
         <h1>My Wall</h1>
+        <button type="button" onClick={this.onAddImageClick} >Add Image</button>
         <ImageWall data={data} />
+        {this.state.show_new_image_dialogue?
+          <NewImageDialogue onCancel={this.onNewImageCancel}
+            onOK={this.onNewImageOK} />
+          :null
+        }
       </div>
     );
   };
