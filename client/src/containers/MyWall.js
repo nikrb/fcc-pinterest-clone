0710a7446d961a948b58b7fa08a079498a8bc747
nikrb <nik_rb@yahoo.com>
2017-09-20 01:36:34 +0100

    remove old dummy data

--- client/src/containers/MyWall.js
+++ client/src/containers/MyWall.js
@@ -2,10 +2,22 @@ import React from 'react';
 import Auth from '../modules/Auth';
 import ImageWall from '../components/ImageWall';
 import NewImageDialogue from '../components/NewImageDialogue';
+import {getUserImages} from './PImageActions';
 
 export default class MyWall extends React.Component {
   state = {
+    image_list: [],
     show_new_image_dialogue: false
+  };
+  componentWillMount = () => {
+    const owner = Auth.get_id();
+    getUserImages( {owner, limit:10, offset: 0})
+    .then( (response) => {
+      console.log( "get user images response:", response);
+      if( response.data){
+        this.setState( {image_list: response.data});
+      }
+    });
   };
   onAddImageClick = (e) => {
     this.setState( {show_new_image_dialogue: true});
@@ -17,20 +29,14 @@ export default class MyWall extends React.Component {
     // create new image for user
     console.log( "add new image:", image_url);
     this.setState( {show_new_image_dialogue: false});
+
   };
   render = () => {
-    const user_id = Auth.get_id();
-    // generate a list of pics
-    const bu = "https://via.placeholder.com/";
-    const data = [
-      bu+"350x100", "bloopydoo", bu+"100x350", bu+"350x100", bu+"250x250", bu+"250x350",
-      bu+"250x150", bu+"100x100", bu+"100x100", bu+"100x100", bu+"100x100"
-    ];
     return (
       <div className="App">
         <h1>My Wall</h1>
         <button type="button" onClick={this.onAddImageClick} >Add Image</button>
-        <ImageWall data={data} />
+        <ImageWall data={this.state.image_list} />
         {this.state.show_new_image_dialogue?
           <NewImageDialogue onCancel={this.onNewImageCancel}
             onOK={this.onNewImageOK} />
