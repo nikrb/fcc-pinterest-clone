c02b0c1400ecff1a498bd3c6225c636670f0eeab
nikrb <nik_rb@yahoo.com>
2017-09-20 01:32:40 +0100

    fix image default

--- client/src/components/ImageDefault.js
+++ client/src/components/ImageDefault.js
@@ -2,47 +2,27 @@ import React from 'react';
 
 export default class ImageDefault extends React.Component {
   state = {
-    image: null,
-    missing_url: null,
-    show_image: false
+    image_error: false
   };
-  componentWillMount = () => {
-    const {src, missing_url} = this.props;
-    this.getImage( src)
-    .then( ( img) => {
-      console.log( "got image:", img.src);
-      this.setState( {image:img, show_image: true});
-    })
-    .catch( (err) => {
-      console.error( "image didn't load:", err);
-      this.setState( {show_image: true});
-    });
-    this.setState( {missing_url});
+  componentWillReceiveProps = (props) => {
+    if( props.src !== this.props.src){
+      this.setState( {image_error: false});
+    }
   };
-  getImage = ( url) => {
-    console.log( "get url:", url);
-    return new Promise( (resolve, reject) => {
-      const timeout_id = setTimeout( () => { reject( {error:"image timed out"})}, 5000);
-      const img = new Image();
-      img.onload = () => {
-        clearTimeout( timeout_id);
-        resolve (img);
-      };
-      img.onerror = () => {
-        clearTimeout( timeout_id);
-        reject( {error:"image not found"});
-      };
-      img.src = url;
-    });
+  onImageError = (e) => {
+    console.log( "image error for url:", e.target.src);
+    this.setState( {image_error: true});
   };
   render = () => {
-    const {show_image, image, missing_url} = this.state;
+    const {src,missing_url, style} = this.props;
+    const {image_error} = this.state;
+    const url = src?src:missing_url;
     return (
       <div>
-        {show_image?
-          <img className="item" src={image?image.src:missing_url} alt="" />
+        {image_error?
+          <img style={style} src={missing_url} alt="noimage"/>
           :
-          <div className="item" />
+          <img style={style} src={url} alt="noimage" onError={this.onImageError} />
         }
       </div>
     );
