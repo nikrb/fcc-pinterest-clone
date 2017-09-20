11f5bb3316d5abaf073bea6a94a42414ef21e25d
nikrb <nik_rb@yahoo.com>
2017-09-20 02:04:02 +0100

    rename addimageclick and enc/dec url and title

--- client/src/containers/MyWall.js
+++ client/src/containers/MyWall.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Auth from '../modules/Auth';
 import ImageWall from '../components/ImageWall';
 import NewImageDialogue from '../components/NewImageDialogue';
-import {getUserImages} from './PImageActions';
+import {getUserImages,createNewImage} from './PImageActions';
 
 export default class MyWall extends React.Component {
   state = {
@@ -15,27 +15,37 @@ export default class MyWall extends React.Component {
     .then( (response) => {
       console.log( "get user images response:", response);
       if( response.data){
-        this.setState( {image_list: response.data});
+        const data = response.data.map( (i)=>{
+          return {...i, url: decodeURIComponent( i.url),
+            title: decodeURIComponent( i.title)};
+        });
+        this.setState( {image_list: data});
       }
     });
   };
-  onAddImageClick = (e) => {
+  onShowNewImageDlg = (e) => {
     this.setState( {show_new_image_dialogue: true});
   };
   onNewImageCancel = () => {
     this.setState( {show_new_image_dialogue: false});
   };
-  onNewImageOK = (image_url) => {
+  onNewImageOK = (detail) => {
     // create new image for user
-    console.log( "add new image:", image_url);
+    console.log( "add new image:", detail);
     this.setState( {show_new_image_dialogue: false});
-
+    const payload = {url:encodeURIComponent( detail.url),
+      title:encodeURIComponent( detail.title),
+      owner:Auth.get_id(), created: new Date()};
+    createNewImage( payload)
+    .then( (response) => {
+      console.log( "create new image response:", response);
+    });
   };
   render = () => {
     return (
       <div className="App">
         <h1>My Wall</h1>
-        <button type="button" onClick={this.onAddImageClick} >Add Image</button>
+        <button type="button" onClick={this.onShowNewImageDlg} >Add Image</button>
         <ImageWall data={this.state.image_list} />
         {this.state.show_new_image_dialogue?
           <NewImageDialogue onCancel={this.onNewImageCancel}
