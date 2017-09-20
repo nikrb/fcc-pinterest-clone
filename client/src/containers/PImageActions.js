bf3cf4017ba6410d32ccd235aa57175615682307
nikrb <nik_rb@yahoo.com>
2017-09-20 01:36:55 +0100

    implement image actions

---
+++ client/src/containers/PImageActions.js
@@ -0,0 +1,39 @@
+import Auth from '../modules/Auth';
+import {checkStatus, parseJSON} from '../modules/util';
+
+export function getAllImages( payload){
+  return fetch( '/apo/pimages', {
+    method: 'post',
+    body: JSON.stringify( payload)
+  })
+  .then( checkStatus)
+  .then( parseJSON);
+}
+
+export function getUserImages( payload){
+  return fetch( '/api/userimages', {
+    method: 'post',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': `bearer ${Auth.getToken()}`
+    },
+    body: JSON.stringify( payload)
+  })
+  .then( checkStatus)
+  .then( parseJSON);
+}
+
+export function createNewImage( payload){
+  return fetch( '/api/userimage', {
+    method: 'post',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': `bearer ${Auth.getToken()}`
+    },
+    body: JSON.stringify( payload)
+  })
+  .then( checkStatus)
+  .then( parseJSON);
+}
