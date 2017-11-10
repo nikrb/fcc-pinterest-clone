fd7fee684e1a4ee2ad779e15b5211fbd1750ced3
nikrb <nik_rb@yahoo.com>
2017-11-10 19:56:00 +0000

    remove process nexttick

--- server/passport/twitter-strategy.js
+++ server/passport/twitter-strategy.js
@@ -15,21 +15,19 @@ module.exports = function(){
     callbackURL: callback_url // config.twitter.callbackURL
   },
   function(accessToken, refreshToken, profile, done) {
-    process.nextTick( function(){
-      var searchQuery = {
-        twitterId: profile.id
-      };
-      var update = {
-        twitterId: profile.id,
-        name: profile.displayName,
-        email:profile.email
-      };
-      var updateOptions = {
-        upsert: true
-      };
-      User.findOneAndUpdate(searchQuery, update, updateOptions, function(err, user) {
-          return done(err, user);
-      });
+    var searchQuery = {
+      twitterId: profile.id
+    };
+    var update = {
+      twitterId: profile.id,
+      name: profile.displayName,
+      email:profile.email
+    };
+    var updateOptions = {
+      upsert: true
+    };
+    User.findOneAndUpdate(searchQuery, update, updateOptions, function(err, user) {
+        return done(err, user);
     });
   }));
 }
