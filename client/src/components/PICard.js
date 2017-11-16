1091aa9d01b44928886643c8a547012ce53d6c96
nikrb <nik_rb@yahoo.com>
2017-11-16 00:35:15 +0000

    refactor pimage card

---
+++ client/src/components/PICard.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Author from './Author';
+import {FavouriteButton} from './Favourite';
+import DeleteButton from './DeleteButton';
+import ImageDefault from './ImageDefault';
+
+export default class PICard extends React.Component {
+  render = () => {
+    const card_style = {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      padding: "5px"
+    };
+    const image_style = {
+      maxWidth: "100%"
+    };
+    const button_wrapper = {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      width: "90%"
+    };
+    const { author, title, authorClicked, image_src, onDeleteClicked, data_id,
+      onFavouriteClicked, favourite_count} = this.props;
+    return (
+      <div>
+        <div className="item" >
+        <div style={card_style} >
+          <ImageDefault style={image_style} src={image_src}
+            missing_url="//via.placeholder.com/200x100?text=noimage"/>
+          <span>{title}</span>
+          <Author author={author} authorClicked={authorClicked} />
+          <div style={button_wrapper}>
+            {onDeleteClicked?
+              <DeleteButton onClick={onDeleteClicked} data={data_id} />
+              // blank div to keep favourite button on right
+              :<div/>
+            }
+            <FavouriteButton data={data_id}
+              addFavourite={onFavouriteClicked}
+              favourite_count={favourite_count||0} />
+          </div>
+        </div>
+        </div>
+      </div>
+    );
+  };
+};
--- client/src/components/PIGrid.js
+++ client/src/components/PIGrid.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import Author from './Author';
-import {FavouriteButton} from './Favourite';
-import DeleteButton from './DeleteButton';
-import ImageDefault from './ImageDefault';
+import PICard from "./PICard";
 
 export default class PIGrid extends React.Component {
   componentWillMount = () => {
@@ -10,42 +7,15 @@ export default class PIGrid extends React.Component {
   };
   render = () => {
     const {data} = this.props;
-    const card_style = {
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      padding: "5px"
-    };
-    const image_style = {
-      maxWidth: "100%"
-    };
-    const button_wrapper = {
-      display: "flex",
-      flexDirection: "row",
-      justifyContent: "space-between",
-      alignItems: "center",
-      width: "90%"
-    };
     const pix = data.map( (p,i) => {
       return (
-        <div className="item" key={i} >
-          <div style={card_style} >
-            <ImageDefault style={image_style} src={p.url}
-              missing_url="//via.placeholder.com/200x100?text=noimage"/>
-            <span>{p.title}</span>
-            <Author author={p.owner} authorClicked={this.props.authorClicked} />
-            <div style={button_wrapper}>
-              {this.props.onDeleteClicked?
-                <DeleteButton onClick={this.props.onDeleteClicked} data={p._id} />
-                // blank div to keep favourite button on right
-                :<div/>
-              }
-              <FavouriteButton data={p._id}
-                addFavourite={this.props.onFavouriteClicked}
-                favourite_count={p.favourites.length||0} />
-            </div>
-          </div>
-        </div>
+        <PICard key={i} {...this.props}
+          image_src={p.url}
+          title={p.title}
+          author={p.owner}
+          data_id={p._id}
+          addFavourite={this.props.onFavouriteClicked}
+          favourite_count={p.favourites.length || 0} />
       );
     });
     return (
